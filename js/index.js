1428f06a3da7cc876fd9b91bb21ae01a88cc7e54
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-03-21 20:27:59 +0100

    js tipos de datos

---
+++ js/index.js
@@ -0,0 +1,20 @@
+console.log('Hola, Mundo')
+//TIPOS DE DATOS:
+//Esto es un comentario. tipos de datos en JS
+//String es una cadena de caracteres(una frase, una palabra o incluso una sola letra).Tenemos que utilizar comillas simples '' o dobles ""
+//booleans: true o false
+//Null_ nulo_es un valor(por lo general hace referencia que una variable se encuentra vacia), pero esta definida y su valor es nulo)
+//Number_Muy importante!!! Los numbers no utilizan comillas, si lo ves entre comillas es un string
+//Undefined._una variable que no se encuentra definida
+//Object=objeto, puede tener cualquier combinacion de los datos anteriores, son estructuras que nos permite agrupar todos tipos de datos de los anteriores
+
+//Definicion Variables: var, let, const
+
+//var_evita utilizarla
+
+let miPrimeraVariable = "Mi primera variable" // mi variable let es un string. Let es una palabra reservada por js
+console.log(miPrimeraVariable); // Si queremos llamar la variable, en console no ponemos comillas, si las ponemos js piensa que llamamos al string.
+
+//Reasignando valor a la variable:
+miPrimeraVariable = 'esto ha cambiado' //Mi primera variable ha cambiado a un string, le hemos reasignado un valor
+console.log(miPrimeraVariable);
